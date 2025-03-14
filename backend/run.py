b6d2c4fbdc56bf4892775cd1a032c5d64066fc59
L34NDP0 <105683560+L34NDP0@users.noreply.github.com>
2025-03-14 13:53:55 -0300

    Update run.py
    Adicionando porta para deploy no render

--- backend/run.py
+++ backend/run.py
@@ -6,4 +6,11 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="localhost", port=8051, debug=True)
+    
+    
+#if __name__ == '__main__':
+ #   with app.app_context():
+  #      db.create_all()
+   # port = int(os.environ.get("PORT", 8051))
+    #app.run(host="localhost", port=port, debug=True)
