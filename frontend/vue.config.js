bc93eba75b0a64b579f56afe2b62b2d5a681b421
L34NDP0 <105683560+L34NDP0@users.noreply.github.com>
2025-03-17 13:41:19 -0300

    Updates memory

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,21 @@
 // frontend/vue.config.js
+
 const { defineConfig } = require('@vue/cli-service')
 
-
-module.exports = {
-  runtimeCompiler: true,
-  lintOnSave: false,
-  transpileDependencies: [],
-  devServer: { devMiddleware: { writeToDisk: true } }
-};
-
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 250000
+      }
+    }
+  }
+})
