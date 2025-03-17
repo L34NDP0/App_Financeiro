cb11d1d318ae424f77d5ddc3e6b4d5c084bb0a8c
L34NDP0 <105683560+L34NDP0@users.noreply.github.com>
2025-03-17 00:22:42 -0300

    New Update

--- backend/run.py
+++ backend/run.py
@@ -4,10 +4,12 @@ import os
 
 app = create_app()
 
-#criando banco de dados
-with app.app_context():
-        db.create_all()
-        
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))    
-    app.run(host="localhost", port=port, debug=True)
+    with app.app_context():
+        db.create_all()
+    port = int(os.environ.get("PORT", 10000))    
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
