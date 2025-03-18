60761f9b31925a74fe347b1b9da8215e1b78f67a
L34NDP0 <105683560+L34NDP0@users.noreply.github.com>
2025-03-18 13:35:19 -0300

    update link produção

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 // Configuração da porta e base URL
 //const port = process.env.PORT || 8080
-//const API_URL = process.env.VUE_APP_API_URL || `https://app-financeiro-2q7m.onrender.com:${port}`
-const API_URL = process.env.VUE_APP_API_URL || `localhost:10000`
+const API_URL = process.env.VUE_APP_API_URL || `https://app-financeiro-2q7m.onrender.com:${port}`
+//const API_URL = process.env.VUE_APP_API_URL || `localhost:10000`
 
 // Configuração global do Axios
 axios.defaults.baseURL = API_URL
